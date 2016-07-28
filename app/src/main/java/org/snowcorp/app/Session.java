19bd2967ef970dd8d6cda7f162f0bf02525a19cc
Akshay Raj <snowcorp2@gmail.com>
2016-07-29 03:00:23 +0530

    Initial commit

---
+++ app/src/main/java/org/snowcorp/app/Session.java
@@ -0,0 +1,39 @@
+package org.snowcorp.app;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Akshay Raj on 7/28/2016.
+ * Snow Corporation Inc.
+ * www.snowcorp.org
+ */
+public class Session {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "snow-intro-slider";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public Session(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+}
